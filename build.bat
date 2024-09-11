2a6320192277787dbb497a7fb416df9552c7ca6e
Johannes Förstner <johannes@famfoerstner.de>
2024-09-11 17:46:37 +0200

    More convenient scripting for pre-build checks

--- build.bat
+++ build.bat
@@ -16,9 +16,12 @@ set ds=%DungeonSiege%
 set tc=%TankCreator%
 
 :: pre-build checks
+robocopy "%bits%\original\templates" "%bits%\world\contentdb\templates\original" /S
 pushd %gaspy%
 venv\Scripts\python -m build.pre_build_checks %map% --check standard --bits "%bits%"
-if %errorlevel% neq 0 pause
+set pre_build_checks_errorlevel=%errorlevel%
+rmdir /S /Q "%bits%\world\contentdb\templates\original"
+if %pre_build_checks_errorlevel% neq 0 pause
 popd
 
 :: Compile map file
