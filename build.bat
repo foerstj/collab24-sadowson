4e4a3e2eadcb4405b11dd6bb1380cb81a2087edd
Johannes Förstner <johannes@famfoerstner.de>
2025-10-26 11:07:28 +0100

    Have the original templates right in the templates folder

--- build.bat
+++ build.bat
@@ -19,12 +19,9 @@ set tc=%TankCreator%
 :: pre-build checks
 setlocal EnableDelayedExpansion
 if not "%gaspy%"=="" (
-  robocopy "%bits%\original\templates" "%bits%\world\contentdb\templates\original" /S
   pushd %gaspy%
   venv\Scripts\python -m build.pre_build_checks %map% --check standard --bits "%bits%"
-  set pre_build_checks_errorlevel=!errorlevel!
-  rmdir /S /Q "%bits%\world\contentdb\templates\original"
-  if !pre_build_checks_errorlevel! neq 0 pause
+  if !errorlevel! neq 0 pause
   popd
 )
 endlocal
